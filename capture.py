17f0965e337c1ba2bbe9b79f831db1a911f457fd
nkaw0009 <50653351+neviskawatra@users.noreply.github.com>
2025-03-05 04:41:15 +1100

    Vector implementation and thumb angle

--- capture.py
+++ capture.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe
+from math_utils import Vector
 
 mp_hands = mediapipe.solutions.hands
 hands = mp_hands.Hands()
@@ -19,6 +20,13 @@ while cam.isOpened():
     if processed_frame.multi_hand_landmarks:
         for landmarks in processed_frame.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+            thumb_base = landmarks.landmark[mp_hands.HandLandmark.THUMB_CMC]
+            thumb_tip = landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP] 
+            
+            thumb_vector = [thumb_base.x - thumb_tip.x, thumb_base.y - thumb_tip.y]
+            unit_vector = Vector.unit_vector(thumb_vector)
+            
+            cv2.putText(frame, f"Thumb vector: {unit_vector}", (50, 50), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 2)
             
     cv2.imshow("Test", frame)
     
---
+++ math_utils.py
@@ -0,0 +1,17 @@
+import math
+
+class Vector:
+    
+    @classmethod
+    def calculate_angle(cls, v1, v2):
+        dot_product = v1[0] * v2[0] + v1[1] * v2[1]
+        magnitude_v1 = math.sqrt(v1[0]**2 + v1[1]**2)
+        magnitude_v2 = math.sqrt(v2[0]**2 + v2[1]**2)
+        cos_theta = dot_product / (magnitude_v1 * magnitude_v2)
+        angle = math.acos(cos_theta)
+        return math.degrees(angle)
+    
+    @classmethod
+    def unit_vector(cls, v):
+        magnitude = math.sqrt(v[0]**2 + v[1]**2)
+        return (v[0] / magnitude, v[1] / magnitude)
